2bd1324dcf90a38bedc76a309412328d480074e7
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-04-05 14:12:54 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@7208 fa64baa9-71d1-4fed-97ae-c15534abce97

--- include/Utils/FeatureStatus.h
+++ include/Utils/FeatureStatus.h
@@ -42,7 +42,7 @@ namespace gezi {
 			for (uint64 i = 0; i < instanceNum; i++)
 			{
 				pb.progress(i);
-				Instance instance = *instances[i];
+				InstancePtr& instance = instances[i];
 				if (instance->IsPositive())
 				{
 					instance->features.ForEach([&](int index, double value)
