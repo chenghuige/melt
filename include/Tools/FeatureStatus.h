3bd7dec5070fa84669587552cb301991a976524d
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-04-05 00:57:04 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@7159 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ include/Tools/FeatureStatus.h
@@ -0,0 +1,36 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   Tools/FeatureStatus.h
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-04-04 15:37:08.135276
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef TOOLS__FEATURE_STATUS_H_
+#define TOOLS__FEATURE_STATUS_H_
+
+namespace gezi {
+
+class FeatureStatus 
+{
+public:
+	FeatureStatus() 
+	{
+
+	}
+
+
+
+protected:
+private:
+
+};
+
+}  //----end of namespace gezi
+
+#endif  //----end of TOOLS__FEATURE_STATUS_H_
