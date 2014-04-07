73402a67f9a661e022d8c96d10e99c0056c0e8e6
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-04-07 13:50:22 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@7284 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ include/Predictors/LinearPredictor.h
@@ -0,0 +1,34 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   Predictors/LinearPredictor.h
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-04-07 21:50:18.354006
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef PREDICTORS__LINEAR_PREDICTOR_H_
+#define PREDICTORS__LINEAR_PREDICTOR_H_
+
+namespace gezi {
+
+class LinearPredictor 
+{
+public:
+	LinearPredictor() 
+	{
+
+	}
+
+protected:
+private:
+
+};
+
+}  //----end of namespace gezi
+
+#endif  //----end of PREDICTORS__LINEAR_PREDICTOR_H_
