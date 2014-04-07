da5f9c6aec87830985bb2421bd6c61e8f4a80603
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-04-07 07:03:27 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@7241 fa64baa9-71d1-4fed-97ae-c15534abce97

--- include/Numeric/Vector/Vector.h
+++ include/Numeric/Vector/Vector.h
@@ -466,13 +466,13 @@ namespace gezi {
 				}
 				else
 				{
-					for (int i = 0; i < values.length; i++)
+					for (size_t i = 0; i < values.size(); i++)
 						values[i] = 0;
 				}
 			}
 			else
 			{
-				for (int i = 0; i < values.length; i++)
+				for (size_t i = 0; i < values.size(); i++)
 					values[i] *= d;
 			}
 		}
