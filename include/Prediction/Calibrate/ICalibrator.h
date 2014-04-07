4b1eb372c20a891aadb6728a9635a1af5b5a8a54
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-04-07 12:53:10 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@7279 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ include/Prediction/Calibrate/ICalibrator.h
@@ -0,0 +1,34 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   Prediction/Calibrate/ICalibrator.h
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-04-07 20:52:24.932084
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef PREDICTION__CALIBRATE__I_CALIBRATOR_H_
+#define PREDICTION__CALIBRATE__I_CALIBRATOR_H_
+
+namespace gezi {
+
+class ICalibrator 
+{
+public:
+	ICalibrator() 
+	{
+
+	}
+
+protected:
+private:
+
+};
+
+}  //----end of namespace gezi
+
+#endif  //----end of PREDICTION__CALIBRATE__I_CALIBRATOR_H_
