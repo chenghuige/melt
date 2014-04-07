dedb6c4f697295beb5bba790c0755548e1862a1a
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-04-07 14:07:34 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@7285 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ include/MLCore/Predictor.h
@@ -0,0 +1,34 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   MLCore/Predictor.h
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-04-07 22:07:30.510777
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef M_L_CORE__PREDICTOR_H_
+#define M_L_CORE__PREDICTOR_H_
+
+namespace gezi {
+
+class Predictor 
+{
+public:
+	Predictor() 
+	{
+
+	}
+
+protected:
+private:
+
+};
+
+}  //----end of namespace gezi
+
+#endif  //----end of M_L_CORE__PREDICTOR_H_
