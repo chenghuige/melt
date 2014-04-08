ec5c374b1404622a05d29068f61359fb55a3c492
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-04-08 11:37:57 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@7312 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ src/Prediction/Trainers/LinearSVM.cpp
@@ -0,0 +1,30 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   LinearSVM.cpp
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-04-08 19:36:17.509808
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef LINEAR_S_V_M_CPP_
+#define LINEAR_S_V_M_CPP_
+
+namespace gezi {
+
+class LinearSVM 
+{
+public:
+
+protected:
+private:
+
+};
+
+}  //----end of namespace gezi
+
+#endif  //----end of LINEAR_S_V_M_CPP_
