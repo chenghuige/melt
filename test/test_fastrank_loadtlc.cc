8b7b04c5844b85fb7df53999eab26782558ddfe7
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-04-13 15:25:51 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@7473 fa64baa9-71d1-4fed-97ae-c15534abce97

--- test/test_fastrank_loadtlc.cc
+++ test/test_fastrank_loadtlc.cc
@@ -24,6 +24,7 @@ DEFINE_int32(level, 0, "min log level");
 //DEFINE_string(o, "", "output");
 DECLARE_string(i);
 DECLARE_string(o);
+DEFINE_string(fe, "", "input feature");
 DEFINE_string(type, "simple", "");
 
 TEST(fastrank_loadtlc, func)
@@ -31,7 +32,7 @@ TEST(fastrank_loadtlc, func)
 	FastRankPredictor predictor;
 	predictor.Load(FLAGS_i);
 
-	Vector fe("0	0	0	5.64897	1	0	1	1	0	1	6	10	6	0	6	2   0.693147	0	0	0.693147	6.85307	1.98208	5.51745	0	0	0	0	0	0	0	0	0	1	0   0.75	0.75	0	3.18324	0.16643	0	0	12	0	0.0769231	0.239049	0	0	3.18324	0.16643	4.4	0   4.4	14.0063	4.4	4.4	0	0	0	0	0	0	0	0	1	1	1	1	0.7	0	0   2.5	2.5	2.5	2.5	2.5	2.5	2.5	2.5	2.5	10	-0.88	0	-0.88	-0.89	0.000111111	-3	0	-0.80.000111111	0	1	0	0	0	1	0	0	0	1	0	0	-0.89	-0.886667	-0.89	-0.888	1	1	1	1	1	1	1	1	1	1	0	0.00996016	0	10	0	2	0   2.5	5.75	0.1	1	0	1	0	0	0	0	0	0	0	0	1	0	10	10");
+	Vector fe(FLAGS_fe);
 
 	Pval(fe[137]);
 
