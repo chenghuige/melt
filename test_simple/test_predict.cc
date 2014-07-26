dd80e58595e2dddff4d3e4efa39af9a2701354e4
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-07-26 02:47:39 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@9605 fa64baa9-71d1-4fed-97ae-c15534abce97

--- test_simple/test_predict.cc
+++ test_simple/test_predict.cc
@@ -19,6 +19,18 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-   
-  return 0;
+	using gezi::simple::Predictor;
+	using gezi::simple::PredictorFactory;
+	Predictor predictor = PredictorFactory::LoadPredictor("./thread.model");
+	
+	int indices_[4] = { 1, 3, 5, 7 };
+	double values_[4] = { 1.2, 3.1, 4.2, 1.1 };
+	vector<int> indices(indices_, indices_ + 4);
+	vector<double> values(values_, values_ + 4);
+
+	double probability = predictor.Predict(indices, values);
+
+	cout << "probability: " << probability << endl;
+
+	return 0;
 }
