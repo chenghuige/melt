0bffb784536e1cfc15689e1e99a11d912ad60113
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-07-26 01:34:16 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@9593 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ src/Simple/Predictor.cpp
@@ -0,0 +1,28 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   Predictor.cpp
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-07-26 09:34:06.790060
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef PREDICTOR_CPP_
+#define PREDICTOR_CPP_
+
+
+class Predictor 
+{
+public:
+
+protected:
+private:
+
+};
+
+
+#endif  //----end of PREDICTOR_CPP_
---
+++ src/Simple/PredictorFactory.cpp
@@ -0,0 +1,28 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   PredictorFactory.cpp
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-07-26 09:34:14.603965
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef PREDICTOR_FACTORY_CPP_
+#define PREDICTOR_FACTORY_CPP_
+
+
+class PredictorFactory 
+{
+public:
+
+protected:
+private:
+
+};
+
+
+#endif  //----end of PREDICTOR_FACTORY_CPP_
