b7650b7eefee515ea298cc3ead0d9727ad9b2535
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-07-26 01:37:57 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@9596 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ test_simple/test_predict.cc
@@ -0,0 +1,23 @@
+/** 
+ *  ==============================================================================
+ * 
+ *          \file   test_predict.cc
+ *
+ *        \author   chenghuige  
+ *
+ *          \date   2014-07-26 09:35:27.230220
+ *  
+ *   \Description  
+ *
+ *  ==============================================================================
+ */
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    
+  return 0;
+}
