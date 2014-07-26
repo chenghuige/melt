5700f0440ee6b0824cfa5e591752d2f140fe1561
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-07-26 01:05:03 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@9588 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ include/PythonWrapper/PyPredictor.h
@@ -0,0 +1,30 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   PyPredictor.h
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-07-26 09:04:24.448768
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef PY_PREDICTOR_H_
+#define PY_PREDICTOR_H_
+
+namespace gezi {
+
+class PyPredictor 
+{
+public:
+
+protected:
+private:
+
+};
+
+}  //----end of namespace gezi
+
+#endif  //----end of PY_PREDICTOR_H_
---
+++ include/PythonWrapper/PyPredictorFactory.h
@@ -0,0 +1,30 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   PyPredictorFactory.h
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-07-26 09:04:34.217769
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef PY_PREDICTOR_FACTORY_H_
+#define PY_PREDICTOR_FACTORY_H_
+
+namespace gezi {
+
+class PyPredictorFactory 
+{
+public:
+
+protected:
+private:
+
+};
+
+}  //----end of namespace gezi
+
+#endif  //----end of PY_PREDICTOR_FACTORY_H_
