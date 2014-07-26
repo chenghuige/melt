475af53f2fdb75584a3b28a12f3930dc11d10b36
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-07-26 03:09:45 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@9611 fa64baa9-71d1-4fed-97ae-c15534abce97

--- test_simple/test_predict.cc
+++ test_simple/test_predict.cc
@@ -1,13 +1,13 @@
-/** 
+/**
  *  ==============================================================================
- * 
+ *
  *          \file   test_predict.cc
  *
- *        \author   chenghuige  
+ *        \author   chenghuige
  *
  *          \date   2014-07-26 09:35:27.230220
- *  
- *   \Description  
+ *
+ *   \Description
  *
  *  ==============================================================================
  */
@@ -22,15 +22,29 @@ int main(int argc, char *argv[])
 	using gezi::simple::Predictor;
 	using gezi::simple::PredictorFactory;
 	Predictor predictor = PredictorFactory::LoadPredictor("./thread.model");
-	
-	int indices_[4] = { 1, 3, 5, 7 };
-	double values_[4] = { 1.2, 3.1, 4.2, 1.1 };
-	vector<int> indices(indices_, indices_ + 4);
-	vector<double> values(values_, values_ + 4);
 
-	double probability = predictor.Predict(indices, values);
+	{
+		int indices_[4] = { 1, 3, 5, 7 };
+		double values_[4] = { 1.2, 3.1, 4.2, 1.1 };
+		vector<int> indices(indices_, indices_ + 4);
+		vector<double> values(values_, values_ + 4);
 
-	cout << "probability: " << probability << endl;
+		double probability = predictor.Predict(indices, values);
+
+		cout << "probability: " << probability << endl;
+	}
+
+
+	{
+		vector<int> indices;
+		vector<double> values;
+		double output = 0;
+		double probability = predictor.Predict(indices, values, output);
+
+		cout << "output: " << output << endl;
+		cout << "probability: " << probability << endl;
+	}
+
 
 	return 0;
 }
