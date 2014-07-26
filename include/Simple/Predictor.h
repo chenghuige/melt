a2147721519a273bbffc532b91aff2226ff56d7a
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-07-26 01:34:21 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@9594 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ include/Simple/Predictor.h
@@ -0,0 +1,30 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   Simple/Predictor.h
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-07-26 09:32:30.332895
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef SIMPLE__PREDICTOR_H_
+#define SIMPLE__PREDICTOR_H_
+
+namespace gezi {
+
+class Predictor 
+{
+public:
+
+protected:
+private:
+
+};
+
+}  //----end of namespace gezi
+
+#endif  //----end of SIMPLE__PREDICTOR_H_
---
+++ include/Simple/PredictorFactory.h
@@ -0,0 +1,30 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   Simple/PredictorFactory.h
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-07-26 09:32:58.616068
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef SIMPLE__PREDICTOR_FACTORY_H_
+#define SIMPLE__PREDICTOR_FACTORY_H_
+
+namespace gezi {
+
+class PredictorFactory 
+{
+public:
+
+protected:
+private:
+
+};
+
+}  //----end of namespace gezi
+
+#endif  //----end of SIMPLE__PREDICTOR_FACTORY_H_
