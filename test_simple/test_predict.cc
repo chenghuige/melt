eaf20dbb4ad56e7848f82fdfa92f1b504c460505
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-07-26 03:27:35 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@9614 fa64baa9-71d1-4fed-97ae-c15534abce97

--- test_simple/test_predict.cc
+++ test_simple/test_predict.cc
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
 		double output = predictor.Output(indices, values);
 
-		cout << "output: " << probability << endl;
+		cout << "output: " << output << endl;
 	}
 
 
