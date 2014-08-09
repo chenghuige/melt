829b2c2de90977a28279044fb882a557a8041fb8
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-08-09 11:00:32 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@9944 fa64baa9-71d1-4fed-97ae-c15534abce97

--- tools3/PLSA/plsa.cc
+++ tools3/PLSA/plsa.cc
@@ -24,12 +24,53 @@ DEFINE_int32(level, 0, "min log level");
 DEFINE_string(type, "simple", "");
 DEFINE_bool(perf,false, "");
 DEFINE_int32(num, 1, "");
+DEFINE_int32(iter, 20, "iterate num");
 DEFINE_string(i, "", "input file");
+DEFINE_int32(tnum, 100, "topic num");
+DEFINE_string(dict, "./identifer.bin", "input dict file");
 DEFINE_string(o, "", "output file");
 
+#include "Identifer.h"
 void run()
 {
-	PLSAModel plsaModel(100, 1000);
+	Identifer identifer;
+	identifer.Load(FLAGS_dict);
+	PLSAModel plsaModel(FLAGS_tnum, identifer.size());
+
+	ifstream ifs(FLAGS_i);
+	string line;
+	vector<DocInfo> docs;
+	while (getline(ifs, line))
+	{
+		boost::trim(line);
+		svec vec = gezi::split(line, '\t');
+		map<int, int> m;
+		DocInfo doc;
+		for (string key : vec)
+		{
+			int id = identifer.id(key);
+			if (id >= 0)
+			{
+				add_value(m, id, 1);
+				doc.numWords++;
+			}
+		}
+		for (auto& item : m)
+		{
+			doc.content.push_back(make_pair(item.first, item.second));
+		}
+		docs.emplace_back(doc);
+	}
+
+	plsaModel.Train(docs, FLAGS_iter);
+
+	ofstream ofs("result.txt");
+	for (int i = 0; i < FLAGS_tnum; i++)
+	{
+		ofs << "Topic: " << i << endl;
+		plsaModel.PrintTopic(i, identifer, ofs);
+	}
+	
 }
 
 int main(int argc, char *argv[])
