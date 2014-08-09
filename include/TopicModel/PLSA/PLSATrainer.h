f62a34f41eff6bd546ccc2b44d8986916346a7ae
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-08-09 13:48:30 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@9949 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ include/TopicModel/PLSA/PLSATrainer.h
@@ -0,0 +1,30 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   PLSATrainer.h
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-08-09 21:48:13.537936
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef P_L_S_A_TRAINER_H_
+#define P_L_S_A_TRAINER_H_
+
+namespace gezi {
+
+class PLSATrainer 
+{
+public:
+
+protected:
+private:
+
+};
+
+}  //----end of namespace gezi
+
+#endif  //----end of P_L_S_A_TRAINER_H_
