18676481f5d25042673b8c5b0ba4f4a3150c0944
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2015-06-09 10:07:13 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@12957 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ test/test_cv.cc
@@ -0,0 +1,44 @@
+/** 
+ *  ==============================================================================
+ * 
+ *          \file   test_cv.cc
+ *
+ *        \author   chenghuige   
+ *
+ *          \date   2015-06-09 17:51:17.566746
+ *  
+ *  \Description:
+ *
+ *  ==============================================================================
+ */
+
+#define _DEBUG
+#define private public
+#define protected public
+#include "common_util.h"
+
+using namespace std;
+using namespace gezi;
+DEFINE_int32(vl, 5, "vlog level");
+DEFINE_string(i, "", "input");
+DEFINE_string(o, "", "output");
+DEFINE_string(type, "simple", "");
+
+TEST(cv, func)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+  int s = google::ParseCommandLineFlags(&argc, &argv, false);
+  if (FLAGS_log_dir.empty())
+    FLAGS_logtostderr = true;
+  if (FLAGS_v == 0)
+    FLAGS_v = FLAGS_vl;
+  
+  return RUN_ALL_TESTS();
+}
---
+++ test/test_parse_groupkey.cc
@@ -0,0 +1,44 @@
+/** 
+ *  ==============================================================================
+ * 
+ *          \file   test_parse_groupkey.cc
+ *
+ *        \author   chenghuige   
+ *
+ *          \date   2015-06-09 16:41:36.322466
+ *  
+ *  \Description:
+ *
+ *  ==============================================================================
+ */
+
+#define _DEBUG
+#define private public
+#define protected public
+#include "common_util.h"
+
+using namespace std;
+using namespace gezi;
+DEFINE_int32(vl, 5, "vlog level");
+DEFINE_string(i, "", "input");
+DEFINE_string(o, "", "output");
+DEFINE_string(type, "simple", "");
+
+TEST(parse_groupkey, func)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+  int s = google::ParseCommandLineFlags(&argc, &argv, false);
+  if (FLAGS_log_dir.empty())
+    FLAGS_logtostderr = true;
+  if (FLAGS_v == 0)
+    FLAGS_v = FLAGS_vl;
+  
+  return RUN_ALL_TESTS();
+}
