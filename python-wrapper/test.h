5c97dbffe34f1d776c8a26ca890cfc1d456ba4a7
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2015-06-26 08:02:53 +0000

    ISSUE=801434
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@13356 fa64baa9-71d1-4fed-97ae-c15534abce97

--- python-wrapper/test.h
+++ python-wrapper/test.h
@@ -1,28 +1,42 @@
 namespace std
 {
 template<typename x>
-void haha();
+void haha()
+{
+
+}
 }
 template<typename T, typename Args>
 void printf(const char* s, T value, Args args);
 template<typename T,
 typename... Args>
-void printf(const char* s, T value, Args... args);
-void printing();
-//template<typename u>
-//static bool& result();
-//static bool& result();
-//template<typename T,
-//typename U,
-//typename x, typename y,template<
-//typename z
-//>
-//typename... Args>
-//static int* resut2(const char* s, T value, Args... args);
+void printf(const char* s, T value, Args... args)
+{
+
+}
+void printing()
+        {
+        }
+template<typename u>
+static bool& result();
+static bool& result();
+template<typename T,
+typename U,
+typename x, typename y,template<
+typename z
+>
+typename... Args>
+static int* resut2(const char* s, T value, Args... args)
+{
+
+}
 class Abc
 {
-//virtual void abc() = 0;
-bool operator +=(const x);
+virtual void abc() = 0;
+bool operator +=(const x)
+{
+
+}
 int x = 3;
 const static map<string, string> =
 {
@@ -45,14 +59,26 @@ Abc()
 Abc() = default;
 
 Abc() {}
+Abc() {{}}
 
-void play();
-virtual void play2();
+void play()
+{
+    
+}
+virtual void play2()
+{
+
+}
+
 template<typename x,
 typename y>
 void play6(int x,
 int y = 3,
-int z);
+int z)
+{
+    
+}
+
 void play4(int x,
 int y = (3),
 int z)
@@ -63,5 +89,8 @@ int x;
 }
 string play3()
 {(return "good";) }
-const int play5() const;
+const int play5() const{
+    int x = {3};
+    vector<int> v = {1, 2, 3};
+}
 };
