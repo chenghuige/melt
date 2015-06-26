a140f64ae9e07bfdedcf2f0731d86fef0d09a97a
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2015-06-26 12:37:09 +0000

    ISSUE=801434
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@13366 fa64baa9-71d1-4fed-97ae-c15534abce97

--- python-wrapper/format-cplusplus-code.py
+++ python-wrapper/format-cplusplus-code.py
@@ -26,7 +26,7 @@ while i < len(m):
         i += 1
         continue 
 
-    if line != '{' and line != '}' and line.replace(' ', '') != '};' and not line.startswith('namespace') and not line.startswith('#define'):
+    if line != '{' and line != '}' and line.replace(' ', '') != '};' and not line.startswith('namespace') and not line.startswith('#define') and not(i > 1 and m[i -1].strip().endswith('\\')):
         l = []
         for item in line:
             if item == '{' or item == '}':
