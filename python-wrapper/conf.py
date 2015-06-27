1e3b8da7976f233ae9fe588e435d0d5200cc8319
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2015-06-27 05:30:40 +0000

    ISSUE=801434
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@13378 fa64baa9-71d1-4fed-97ae-c15534abce97

--- python-wrapper/conf.py
+++ python-wrapper/conf.py
@@ -3,34 +3,36 @@ define_symbols = ['GCCXML','PYTHON_WRAPPER']
 
 files = [
                                 './gezi.include.python/common_util.h',
-                                #'./include.python/Prediction/Instances/InstanceParser.h',
-                                './gezi.include.python/Numeric/Vector/Vector.h',
-                                './gezi.include.python/Numeric/Vector/WeightVector.h',
-                                './gezi.include.python/Numeric/Vector/vector_util.h',
-                                './gezi.include.python/feature/FeatureVector.h',
-                                './gezi.include.python/feature/features_util.h',
-                                './gezi.include.python/feature/FeatureExtractor.h',
-                                './gezi.include.python/feature/FeaturesExtractorMgr.h',
+                                './include.python/Prediction/Instances/InstanceParser.h',
+                                './include.python/Prediction/Instances/Instance.h',
+                                './include.python/Prediction/Instances/Instances.h',
+                                #'./gezi.include.python/Numeric/Vector/Vector.h',
+                                #'./gezi.include.python/Numeric/Vector/WeightVector.h',
+                                #'./gezi.include.python/Numeric/Vector/vector_util.h',
+                                #'./gezi.include.python/feature/FeatureVector.h',
+                                #'./gezi.include.python/feature/features_util.h',
+                                #'./gezi.include.python/feature/FeatureExtractor.h',
+                                #'./gezi.include.python/feature/FeaturesExtractorMgr.h',
                                 './gezi.include.python/Identifer.h',
                                 './gezi.include.python/Segmentor.h',
-                                './include.python/MLCore/Predictor.h',
-                                './include.python/MLCore/PredictorFactory.h',
-                                './include.python/MLCore/Trainer.h',
-                                './include.python/MLCore/TrainerFactory.h',
-                                './include.python/MLCore/TextPredictor.h',
+                                #'./include.python/MLCore/Predictor.h',
+                                #'./include.python/MLCore/PredictorFactory.h',
+                                #'./include.python/MLCore/Trainer.h',
+                                #'./include.python/MLCore/TrainerFactory.h',
+                                #'./include.python/MLCore/TextPredictor.h',
                                 #'./include.python/PythonWrapper/example.h',
                                 #'./gezi.include.python/Segmentor.h',
 				]
 
 paths = [
-            './gezi.include.python/Numeric/Vector/',
-            './include.python/MLCore/',
-            './include.python/Prediction/Instances/',
+            #'./gezi.include.python/Numeric/Vector/',
+            #'./include.python/MLCore/',
+            #'./include.python/Prediction/Instances/',
         ]
 
 import gezi 
-#for path in paths:
-#    files += [f for f in gezi.get_filepaths(path) if f.endswith('.h')]
+for path in paths:
+    files += [f for f in gezi.get_filepaths(path) if f.endswith('.h')]
 
 root = '/home/users/chenghuige/rsc/'
 
