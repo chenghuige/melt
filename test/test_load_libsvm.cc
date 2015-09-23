098395542fdf18140fb86cb8f97d56a3b3577c18
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2015-09-23 07:41:28 +0000

    ISSUE=1717385
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/melt@14592 fa64baa9-71d1-4fed-97ae-c15534abce97

--- test/test_load_libsvm.cc
+++ test/test_load_libsvm.cc
@@ -16,7 +16,7 @@
 #define private public
 #define protected public
 #include "common_util.h"
-
+#include "Prediction/Instances/instances_util.h"
 using namespace std;
 using namespace gezi;
 DEFINE_int32(vl, 5, "vlog level");
@@ -26,19 +26,19 @@ DEFINE_string(type, "simple", "");
 
 TEST(load_libsvm, func)
 {
-
+	auto instances = create_instances(FLAGS_i);
 }
 
 int main(int argc, char *argv[])
 {
-  testing::InitGoogleTest(&argc, argv);
-  google::InitGoogleLogging(argv[0]);
-  google::InstallFailureSignalHandler();
-  int s = google::ParseCommandLineFlags(&argc, &argv, false);
-  if (FLAGS_log_dir.empty())
-    FLAGS_logtostderr = true;
-  if (FLAGS_v == 0)
-    FLAGS_v = FLAGS_vl;
-  
-  return RUN_ALL_TESTS();
+	testing::InitGoogleTest(&argc, argv);
+	google::InitGoogleLogging(argv[0]);
+	google::InstallFailureSignalHandler();
+	int s = google::ParseCommandLineFlags(&argc, &argv, false);
+	if (FLAGS_log_dir.empty())
+		FLAGS_logtostderr = true;
+	if (FLAGS_v == 0)
+		FLAGS_v = FLAGS_vl;
+	
+	return RUN_ALL_TESTS();
 }
